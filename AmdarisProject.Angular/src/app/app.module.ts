f54703c08eda55fced6e9bc43d7f873b33ffd597
DarkMeg <alexkorman.work@gmail.com>
2022-11-01 18:39:47 +0200

    Fix commit mistake

--- AmdarisProject.Angular/src/app/app.module.ts
+++ AmdarisProject.Angular/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { App3Component } from './app3/app3.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    App3Component
+    AppComponent
   ],
   imports: [
     BrowserModule
