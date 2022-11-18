61fd788e2c5eec4649e21e9af780ec073aff2a4c
DarkMeg <alexkorman.work@gmail.com>
2022-11-18 14:45:59 +0200

    Delete useless import

--- AmdarisProject.Angular/src/app/modules/room/room-view/room-view.module.ts
+++ AmdarisProject.Angular/src/app/modules/room/room-view/room-view.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomViewComponent } from './room-view.component';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
-import { ListItemViewComponent } from 'src/app/common/components/list-item-view/list-item-view.component';
 import { DeleteBtnComponent } from 'src/app/common/components/delete-btn/delete-btn.component';
 import { RoomEditModule } from '../room-edit/room-edit.module';
 
